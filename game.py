a461ed482a4b3d7075dec06da9582510dd863197
sproshchaev <150545997+dproshchaeva@users.noreply.github.com>
2025-02-09 20:43:30 +0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import pygame
 import sys
-
-SCREEN_SIZE = (800, 600)
+import random
+WIDTH, HEIGHT = 800, 600
+BLOCK_SIZE = 10
+WALL_BLOCKS = 3
 GAME_ICON = "snake.png"
 GAME_TITLE = "Snake"
 INITIAL_GAME_SPEED = 10
 BACKGROUND_COLOR = (252, 232, 247)
-APPLES = 3
+INITIAL_APPLES = 3
+INITIAL_SNAKE_LENGTH = 3
+SIZE_X = WIDTH // BLOCK_SIZE - WALL_BLOCKS * 2
+SIZE_Y = HEIGHT // BLOCK_SIZE - WALL_BLOCKS * 2
 
 
 def main():
@@ -23,7 +28,7 @@ def main():
 
 def initialize_pygame():
     pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
+    screen = pygame.display.set_mode(WIDTH, HEIGHT)
     icon = pygame.image.load(GAME_ICON)
     pygame.display.set_icon(icon)
     pygame.display.set_caption(GAME_TITLE)
@@ -66,6 +71,24 @@ def get_events():
 
 
 def update_game_state(events, game_state):
+     check_key_presses(events, game_state)
+     move_snake(game_state)
+     check_collisions(game_state)
+     check_apple_conumption(game_state)
+
+
+def move_snake(game_state):
+    game_state["snake"]
+
+
+def check_collisions(game_state):
+    pass
+
+
+def check_apple_conumption(game_state):
+    pass
+
+def check_key_presses(events, game_state):
     if "quit" in events:
         game_state["program_running"] = False
     elif not game_state["game_running"]:
@@ -83,26 +106,42 @@ def update_game_state(events, game_state):
              if "escape" in events or "space" in events:
                  game_state["game_paused"] = True
              if "up" in events:
-                 pass
+                 game_state["direction"] = (0, -1)
              if "down" in events:
-                 pass
+                 game_state["direction"] = (0, 1)
              if "left" in events:
-                 pass
+                 game_state["direction"] = (-1, 0)
              if "right" in events:
-                 pass
+                 game_state["direction"] = (1, 0)
 
 
 def initialize_new_game(game_state):
-    game_state["apples"] = place_apples(APPLES, game_state)
-    game_state["direction"] = [1, 0]
+    place_snake(INITIAL_SNAKE_LENGTH, game_state)
+    place_apples(INITIAL_APPLES, game_state)
+    game_state["direction"] = (1, 0)
     game_state["game_paused"] = False
     game_state["score"] = 0
     game_state["game_speed"] = INITIAL_GAME_SPEED
 
 
+def place_snake(length, game_state):
+    x = SIZE_X // 2
+    y = SIZE_Y // 2
+    game_state["snake"].append((x, y))
+    for i in range(1, length):
+        game_state["snake"].append(x - i, y)
 
-def place_apples(n, game_state):
-    pass
+
+def place_apples(apples, game_state):
+    game_state["apples"] = []
+    for i in range(apples):
+        x = random.randint(0, SIZE_X - 1)
+        y = random.randint(0, SIZE_Y - 1)
+        while (x, y) in game_state["apples"] or (x, y) in game_state["snake"]:
+            x = random.randint(0, SIZE_X - 1)
+            y = random.randint(0, SIZE_Y - 1)
+        game_state["apples"].append((x, y))
+        
 
 def update_screen(screen, game_state):
     screen.fill(BACKGROUND_COLOR)
