0f2db84873872203966bee1c1c79ffd256b09dc6
sproshchaev <150545997+dproshchaeva@users.noreply.github.com>
2025-02-27 12:20:17 +0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,30 +1,44 @@
 import pygame
 import sys
 import random
-WIDTH = 800
-HEIGHT = 600
-SIZE = WIDTH, HEIGHT
-BLOCK_SIZE = 10
-WALL_BLOCKS = 3
+
+
 GAME_ICON = "snake.png"
 GAME_TITLE = "Snake"
-INITIAL_GAME_SPEED = 10
-BACKGROUND_COLOR = (252, 232, 247)
+
+WIDTH, HEIGHT = 1000, 800
+BLOCK_SIZE = 20
+WALL_BLOCKS = 3
+
+INITIAL_GAME_SPEED = 8
+SPEED_CHANGE = 1
+
+INITIAL_SNAKE_LENGTH = 5
 INITIAL_APPLES = 3
-INITIAL_SNAKE_LENGTH = 3
+
+SNAKE_RADIUS = BLOCK_SIZE // 5
+APPLE_RADIUS = int(BLOCK_SIZE // 2.5)
+EYE_SIZE = BLOCK_SIZE // 7
+FONT_SIZE = int(WALL_BLOCKS * BLOCK_SIZE * 0.75)
+
+WALL_SIZE = BLOCK_SIZE * WALL_BLOCKS
 SIZE_X = WIDTH // BLOCK_SIZE - WALL_BLOCKS * 2
 SIZE_Y = HEIGHT // BLOCK_SIZE - WALL_BLOCKS * 2
-APPLE_COLOR = (139, 0, 0)
-APPLE_RADIUS = BLOCK_SIZE // 4
+
+BACKGROUND_COLOR = (140, 125, 134)
+SNAKE_COLOR = (82, 102, 90)
+EYE_COLOR = (0, 0, 0)
+APPLE_COLOR = (107, 40, 50)
+WALL_COLOR = (31, 31, 31)
+TEXT_COLOR = (5, 5, 5)
 
 
 def main():
     screen, clock = initialize_pygame()
     game_state = initialize_game_state()
     while game_state["program_running"]:
-        clock.tick(game_state['game_speed'])
+        clock.tick(game_state["game_speed"])
         events = get_events()
-        print(events)
         update_game_state(events, game_state)
         update_screen(screen, game_state)
     perform_shutdown()
@@ -32,9 +46,12 @@ def main():
 
 def initialize_pygame():
     pygame.init()
-    screen = pygame.display.set_mode(SIZE)
-    icon = pygame.image.load(GAME_ICON)
-    pygame.display.set_icon(icon)
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    try:
+        icon = pygame.image.load(GAME_ICON)
+        pygame.display.set_icon(icon)
+    except:
+        pass
     pygame.display.set_caption(GAME_TITLE)
     clock = pygame.time.Clock()
     return screen, clock
@@ -46,7 +63,8 @@ def initialize_game_state():
         "game_running": False,
         "game_paused": False,
         "game_speed": INITIAL_GAME_SPEED,
-        "score": 0
+        "score": 0,
+        "max_score": 0
     }
     return game_state
 
@@ -55,7 +73,7 @@ def get_events():
     events = []
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           events.append("quit")
+            events.append("quit")
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 events.append("up")
@@ -76,33 +94,10 @@ def get_events():
 
 def update_game_state(events, game_state):
     check_key_presses(events, game_state)
-    if game_state["game_running"]:
+    if game_state["game_running"] and not game_state["game_paused"]:
         move_snake(game_state)
         check_collisions(game_state)
         check_apple_consumption(game_state)
-
-
-def move_snake(game_state):
-    x = game_state["snake"][0][0] + game_state["direction"][0]
-    y = game_state["snake"][0][1] + game_state["direction"][1]
-    game_state["snake"].insert(0, (x, y))
-
-
-def check_collisions(game_state):
-    x, y = game_state["snake"][0]
-    if x < 0 or y < 0 or x >= SIZE_X or y >= SIZE_Y:
-        game_state["game_running"] = False
-    if len(game_state["snake"]) > len(set(game_state["snake"])):
-        game_state["game_running"] = False
-
-
-def check_apple_consumption(game_state):
-    for apple in game_state["apples"]:
-        if apple == game_state["snake"][0]:
-            game_state["apples"].remove(apple)
-            place_apples(1, game_state)
-            game_state["score"] += 1
-            game_state["game_speed"] = round(game_state["game_speed"] * 1.1)
 
 
 def check_key_presses(events, game_state):
@@ -113,7 +108,7 @@ def check_key_presses(events, game_state):
             game_state["program_running"] = False
         elif "enter" in events:
             initialize_new_game(game_state)
-            game_state["program_running"] = True
+            game_state["game_running"] = True
     elif game_state["game_paused"]:
         if "escape" in events:
             game_state["game_running"] = False
@@ -132,12 +127,43 @@ def check_key_presses(events, game_state):
             game_state["direction"] = (1, 0)
 
 
+def move_snake(game_state):
+    x = game_state["snake"][0][0] + game_state["direction"][0]
+    y = game_state["snake"][0][1] + game_state["direction"][1]
+    game_state["snake"].insert(0, (x, y))
+    if (x, y) not in game_state["apples"]:
+        game_state["snake"].pop()
+
+
+def check_collisions(game_state):
+    x, y = game_state["snake"][0]
+    if (
+            x < 0 or y < 0 or x >= SIZE_X or y >= SIZE_Y or
+            len(game_state["snake"]) > len(set(game_state["snake"]))
+    ):
+        game_state["game_running"] = False
+
+
+def check_apple_consumption(game_state):
+    for apple in game_state["apples"]:
+        if apple == game_state["snake"][0]:
+            game_state["apples"].remove(apple)
+            place_apples(1, game_state)
+            game_state["score"] += 1
+            update_max_score(game_state)
+            game_state["game_speed"] += SPEED_CHANGE
+
+
+def update_max_score(game_state):
+    if game_state["score"] > game_state["max_score"]:
+        game_state["max_score"] = game_state["score"]
+
+
 def initialize_new_game(game_state):
     game_state["snake"] = []
     place_snake(INITIAL_SNAKE_LENGTH, game_state)
-    place_apples(INITIAL_APPLES, game_state)
-    game_state["snake"] = []
     game_state["apples"] = []
+    place_apples(INITIAL_APPLES, game_state)
     game_state["direction"] = (1, 0)
     game_state["game_paused"] = False
     game_state["score"] = 0
@@ -153,7 +179,6 @@ def place_snake(length, game_state):
 
 
 def place_apples(apples, game_state):
-    game_state["apples"] = []
     for i in range(apples):
         x = random.randint(0, SIZE_X - 1)
         y = random.randint(0, SIZE_Y - 1)
@@ -161,7 +186,7 @@ def place_apples(apples, game_state):
             x = random.randint(0, SIZE_X - 1)
             y = random.randint(0, SIZE_Y - 1)
         game_state["apples"].append((x, y))
-        
+
 
 def update_screen(screen, game_state):
     screen.fill(BACKGROUND_COLOR)
@@ -171,18 +196,35 @@ def update_screen(screen, game_state):
         print_game_paused_message(screen)
     else:
         draw_apples(screen, game_state["apples"])
-        draw_snake(screen, game_state["snake"])
+        draw_snake(screen, game_state["snake"], game_state["direction"])
     draw_walls(screen)
     print_score(screen, game_state["score"])
+    print_max_score(screen, game_state["max_score"])
     pygame.display.flip()
 
 
 def print_new_game_message(screen):
-    pass
+    font = pygame.font.SysFont("Courier New", FONT_SIZE, bold=True)
+    text1 = font.render("Press ENTER to start new game", True, TEXT_COLOR)
+    text2 = font.render("Press ESCAPE to quit", True, TEXT_COLOR)
+    text_rect1 = text1.get_rect()
+    text_rect2 = text2.get_rect()
+    text_rect1.center = (WIDTH // 2, HEIGHT // 2 - FONT_SIZE // 2)
+    text_rect2.center = (WIDTH // 2, HEIGHT // 2 + FONT_SIZE // 2)
+    screen.blit(text1, text_rect1)
+    screen.blit(text2, text_rect2)
 
 
 def print_game_paused_message(screen):
-    pass
+    font = pygame.font.SysFont("Courier New", FONT_SIZE, bold=True)
+    text1 = font.render("НАЖМИТЕ SPACE ЧТОБЫ ПРОДОЛЖИТЬ", True, TEXT_COLOR)
+    text2 = font.render("НАЖМИТЕ ESCAPE ЧТОБЫ НАЧАТЬ НОВУЮ ИГРУ", True, TEXT_COLOR)
+    text_rect1 = text1.get_rect()
+    text_rect2 = text2.get_rect()
+    text_rect1.center = (WIDTH // 2, HEIGHT // 2 - FONT_SIZE // 2)
+    text_rect2.center = (WIDTH // 2, HEIGHT // 2 + FONT_SIZE // 2)
+    screen.blit(text1, text_rect1)
+    screen.blit(text2, text_rect2)
 
 
 def draw_apples(screen, apples):
@@ -193,24 +235,71 @@ def draw_apples(screen, apples):
         pygame.draw.rect(screen, APPLE_COLOR, rect, border_radius=APPLE_RADIUS)
 
 
-def perform_shutdown():
-    pygame.init()
-    sys.exit()
+def draw_snake(screen, snake, direction):
+    for segment in snake:
+        x = segment[0] * BLOCK_SIZE + WALL_BLOCKS * BLOCK_SIZE
+        y = segment[1] * BLOCK_SIZE + WALL_BLOCKS * BLOCK_SIZE
+        rect = ((x, y), (BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, SNAKE_COLOR, rect, border_radius=SNAKE_RADIUS)
+    draw_snake_eyes(screen, snake[0], direction)
 
 
-def draw_snake(screen, snake):
-    pass
+def draw_snake_eyes(screen, head, direction):
+    eye_offset = BLOCK_SIZE // 4
+    x, y = direction[0], direction[1]
+    if x == -1 or y == -1:
+        coord_x = head[0] * BLOCK_SIZE + WALL_SIZE + eye_offset
+        coord_y = head[1] * BLOCK_SIZE + WALL_SIZE + eye_offset
+        center = (coord_x, coord_y)
+        pygame.draw.circle(screen, EYE_COLOR, center, EYE_SIZE)
+    if x == -1 or y == 1:
+        coord_x = head[0] * BLOCK_SIZE + WALL_SIZE + eye_offset
+        coord_y = head[1] * BLOCK_SIZE + WALL_SIZE + (BLOCK_SIZE - eye_offset)
+        center = (coord_x, coord_y)
+        pygame.draw.circle(screen, EYE_COLOR, center, EYE_SIZE)
+    if x == 1 or y == -1:
+        coord_x = head[0] * BLOCK_SIZE + WALL_SIZE + (BLOCK_SIZE - eye_offset)
+        coord_y = head[1] * BLOCK_SIZE + WALL_SIZE + eye_offset
+        center = (coord_x, coord_y)
+        pygame.draw.circle(screen, EYE_COLOR, center, EYE_SIZE)
+    if x == 1 or y == 1:
+        coord_x = head[0] * BLOCK_SIZE + WALL_SIZE + (BLOCK_SIZE - eye_offset)
+        coord_y = head[1] * BLOCK_SIZE + WALL_SIZE + (BLOCK_SIZE - eye_offset)
+        center = (coord_x, coord_y)
+        pygame.draw.circle(screen, EYE_COLOR, center, EYE_SIZE)
 
 
 def draw_walls(screen):
-    pass
+    pygame.draw.rect(screen, WALL_COLOR, ((0, 0), (WIDTH, WALL_SIZE)))
+    pygame.draw.rect(screen, WALL_COLOR, ((0, 0), (WALL_SIZE, HEIGHT)))
+    pygame.draw.rect(screen, WALL_COLOR, ((0, HEIGHT - WALL_SIZE), (WIDTH, HEIGHT)))
+    pygame.draw.rect(screen, WALL_COLOR, ((WIDTH - WALL_SIZE, 0), (WIDTH, HEIGHT)))
 
 
 def print_score(screen, score):
-    pass
+    font = pygame.font.SysFont("Courier New", FONT_SIZE, bold=True)
+    text = font.render("Score: " + str(score), True, TEXT_COLOR)
+    text_rect = text.get_rect()
+    text_rect.midleft = (WALL_SIZE, WALL_SIZE // 2)
+    screen.blit(text, text_rect)
 
 
-main()
+def print_max_score(screen, score):
+    font = pygame.font.SysFont("Courier New", FONT_SIZE, bold=True)
+    text = font.render("Hi: " + str(score), True, TEXT_COLOR)
+    text_rect = text.get_rect()
+    text_rect.midright = (WIDTH - WALL_SIZE, WALL_SIZE // 2)
+    screen.blit(text, text_rect)
+
+
+def perform_shutdown():
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
